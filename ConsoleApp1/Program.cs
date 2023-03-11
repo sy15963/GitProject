b3366fbdb2d13baade7224a9283be86a98ce8ec9
Administrator <119095309+sy15963@users.noreply.github.com>
2023-03-11 17:13:08 +0900

    123

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 10;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
